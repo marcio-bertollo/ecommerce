a9e4365d909cd7292cc33d422b260d6ca619c560
Marcio Bertollo <marcio.bertollo@hotmail.com>
2021-07-10 11:15:33 -0300

    cupons e pedidos

--- database/migrations/1625922138525_product_schema.js
+++ database/migrations/1625922138525_product_schema.js
@@ -33,9 +33,26 @@ class ProductSchema extends Schema {
         .inTable('products')
         .onDelete('cascade')      
     })
+
+    this.create('category_product', table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+      table
+        .foreign('category_id')
+        .references('id')
+        .inTable('categories')
+        .onDelete('cascade')
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')      
+    })    
   }
 
   down () {
+    this.drop('category_product')
     this.drop('products')
     this.drop('image_product')
   }
