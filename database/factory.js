45b42c6b14d17db1a8636cbde44dedb594e7a7aa
Marcio Bertollo <marcio.bertollo@hotmail.com>
2021-07-14 22:12:49 -0300

    criação de factory

--- database/factory.js
+++ database/factory.js
@@ -1,21 +1,40 @@
 'use strict'
 
+const Factory = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
-| Factory
+| Fábrica
 |--------------------------------------------------------------------------
 |
-| Factories are used to define blueprints for database tables or Lucid
-| models. Later you can use these blueprints to seed your database
-| with dummy data.
+| As fábricas são usadas para definir projetos para tabelas de banco de dados ou Lucid
+| modelos. Mais tarde, você pode usar esses projetos para propagar seu banco de dados
+| com dados fictícios.
 |
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+ const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+ Factory.blueprint('App/Models/User', (faker) => {
+   return {
+     username: faker.username(),
+     email: faker.email(),
+     password: 'secret'
+   }
+ })
+
+ Factory.blueprint('App/Models/Category', faker => {
+     return {
+         title: faker.country({ full: true }),
+         description: faker.sentence()
+     }
+ })
+
+ Factory.blueprint('App/Models/Product', faker => {
+     return {
+         name: faker.animal(),
+         description: faker.sentence(),
+         price: faker.floating({ min: 0, max: 1000, fixed: 2})
+     }
+ })
